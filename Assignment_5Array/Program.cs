6eee1c5e219698938fdb81fe30899f45772b10d4
visualstudio <visualstudio@230-PC23>
2019-04-18 15:43:06 -0400

    Commit array

--- Assignment_5Array/Program.cs
+++ Assignment_5Array/Program.cs
@@ -10,6 +10,34 @@ namespace Assignment_5Array
     {
         static void Main(string[] args)
         {
+            int[] arr = { 17, 11, 9, 3, 4, 11, 6, 11, 8, 7 };
+            int arr_size = arr.Length;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            printRepeating(arr, arr_size);
+
+        }
+        public static void printRepeating(int[] arr, int size)
+        {
+            int i, j;
+            int counter = 0;
+            Console.WriteLine("Occurs 3 times the array elements are: ");
+            for (i = 0; i < size; i++)
+            {
+                for (j = i + 1; j < size; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        counter++;
+                        Console.Write(arr[i] + " ");
+                    }
+
+                }
+            }
         }
     }
 }
